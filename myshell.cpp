a759fb96ddaaa7f935a572769ce08a390054934b
nathannishi <nnhusky7@uw.edu>
2020-04-21 14:36:17 -0700

    first makefile

--- myshell.cpp
+++ myshell.cpp
@@ -4,20 +4,43 @@
 
 #include <iostream>
 #include <string>
-using namespace std;
+#include <array>
+
 int main() {
+    using namespace std;
 		//cout << "Hello shell" << endl;
-    String line;
+    string line;
     char buf[10][500];
-    while(true) {
+    size_t found;
+    //while(true) {
 		cout << "myshell$";
-		cin >> line;
-		for (int i = 0; i < line.length(); i++) {
-		    for (int j = 0; j )
+		getline(cin,line);
+		cout << "input was: " << line << endl;
+		cout << "line length is " << line.length() << endl;
+		for (int i = 0; i < 10; i++) {  // 10 is hardcoded in, might have to change/ make global var.
+            found = line.find("|");
+            cout << "position of first pipe is " << found << endl;
+            if (found != string::npos) {
+                for (int j = 0; j < found; j++) {
+                    buf[i][j] = line[j];
+                }
+                line.erase(0, found + 2); // +2 because need to index past | and next space
+                cout << "remaining characters in line: " << line << endl;
+                cout << "new line length: " << line.length() << endl;
+            } else {
+                for (int j = 0; j < line.length(); j++) {
+                    buf[i][j] = line[j];
+                }
+                i = 10;
+            }
 		}
-
-
-    }
+        for (int i = 0; i < 10; i++){
+            for (int j = 0; j < 30; j++) {
+                cout << buf[i][j];
+            }
+            cout << endl;
+        }
+    //}
 	return 0;
 }
 
