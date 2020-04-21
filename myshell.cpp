c73020315af0d0b845f0f019138e0e5353d05f26
kelseygbates <kelseygbates@gmail.com>
2020-04-21 14:52:39 -0700

    Made prompt continuously show until user types "quit"

--- myshell.cpp
+++ myshell.cpp
@@ -12,12 +12,13 @@ int main() {
     string line;
     char buf[10][500];
     size_t found;
-    //while(true) {
-		cout << "myshell$";
-		getline(cin,line);
-		cout << "input was: " << line << endl;
-		cout << "line length is " << line.length() << endl;
-		for (int i = 0; i < 10; i++) {  // 10 is hardcoded in, might have to change/ make global var.
+    while(true) {
+			cout << "myshell$";
+			getline(cin,line);
+			if(line == "quit") { break; }
+			cout << "input was: " << line << endl;
+			cout << "line length is " << line.length() << endl;
+			for (int i = 0; i < 10; i++) {  // 10 is hardcoded in, might have to change/ make global var.
             found = line.find("|");
             cout << "position of first pipe is " << found << endl;
             if (found != string::npos) {
@@ -33,14 +34,14 @@ int main() {
                 }
                 i = 10;
             }
-		}
+			}
         for (int i = 0; i < 10; i++){
             for (int j = 0; j < 30; j++) {
                 cout << buf[i][j];
             }
             cout << endl;
         }
-    //}
+    }
 	return 0;
 }
 
