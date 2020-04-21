8dad59a04ca73708bc32240f3d2598f7e9779e9f
nathannishi <nnhusky7@uw.edu>
2020-04-20 18:13:07 -0700

    first makefile

--- myshell.cpp
+++ myshell.cpp
@@ -3,13 +3,21 @@
 //
 
 #include <iostream>
+#include <string>
 using namespace std;
 int main() {
-		cout << "Hello shell" << endl;
-//	while(true) {
-//		cout << "myshell$";
-//
-//	}
+		//cout << "Hello shell" << endl;
+    String line;
+    char buf[10][500];
+    while(true) {
+		cout << "myshell$";
+		cin >> line;
+		for (int i = 0; i < line.length(); i++) {
+		    for (int j = 0; j )
+		}
+
+
+    }
 	return 0;
 }
 
