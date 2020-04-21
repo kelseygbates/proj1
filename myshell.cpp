43c12bb6a80327ad7be49fc7a60c00ba7bd05610
kelseygbates <kelseygbates@gmail.com>
2020-04-20 17:42:19 -0700

    Add myshell.h

--- myshell.cpp
+++ myshell.cpp
@@ -2,3 +2,13 @@
 // Created by Kelsey Bates on 4/20/20.
 //
 
+#include <iostream>
+using namespace std;
+int main() {
+//	while(true) {
+//		cout << "myshell$";
+//
+//	}
+	return 0;
+}
+
---
+++ myshell.h
@@ -0,0 +1,8 @@
+//
+// Created by Kelsey Bates on 4/20/20.
+//
+
+#ifndef PROJ1_MYSHELL_H
+#define PROJ1_MYSHELL_H
+
+#endif //PROJ1_MYSHELL_H
